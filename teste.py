b30e74cc16f1ff2b3d17485ff5b0edb31f548f78
LucasMonteiroSchiavo <lucas.schiavo100@gmail.com>
2025-10-06 14:47:16 -0300

    Sem vasco

--- teste.py
+++ teste.py
@@ -13,12 +13,18 @@ print(f'Idade do aluno: {idade}')
 print(f'Nota da Máteria 1: {nota1}')
 print(f'Nota da Matéria 2: {nota2}')
 print(f'Nota da Matéria 3: {nota3}')
-print(f'Nota da máteria 4: {nota4}')g
+print(f'Nota da máteria 4: {nota4}')
 
 # Verificar resposta e imprimir
 if aluno_passou.lower() == 'sim':
-    print(f'{nome} Passou!')
+    print(f'{nome} Passou! suas Notas foram: \n(nota 1: {nota1}) \n(nota 2: {nota2}) \n(nota 3: {nota3}) \n(nota 4: {nota4})\n Sua média foi de: {(nota1+nota2+nota3+nota4) /4}')
+    print(f'Nome do Aluno: {nome} PARABÉNS')
 elif aluno_passou.lower() == 'nao':
     print(f'{nome} infelizmente não passou!')
 else:
     print('Resposta inválida, digite "sim" ou "nao"')
+
+    print('Vasco')
+
+
+

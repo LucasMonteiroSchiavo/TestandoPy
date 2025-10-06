050f718ea301b70d987075c858a2c718d716f4b2
LucasMonteiroSchiavo <lucas.schiavo100@gmail.com>
2025-10-06 14:14:41 -0300

    Novas alterações 06/10 14:15

--- teste.py
+++ teste.py
@@ -7,16 +7,18 @@ nota3 = 7.5
 nota4 = 8.0
 aluno_passou = input(' O Aluno Passou ? (sim/nao): ')
 
-#Verificar resposta e imprimir
-if aluno_passou.lower() == 'sim':
-    print (f'{nome} Passou!')
-elif aluno_passou.lower() == 'nao':
-    printf(f'{nome} infelizmente não passou!')
-else:
-    print('Resposta inválida, digite "sim" ou "nao"')
 
 print(f'Nome do Aluno: {nome}')
 print(f'Idade do aluno: {idade}')
 print(f'Nota da Máteria 1: {nota1}')
+print(f'Nota da Matéria 2: {nota2}')
+print(f'Nota da Matéria 3: {nota3}')
+print(f'Nota da máteria 4: {nota4}')g
 
-
+# Verificar resposta e imprimir
+if aluno_passou.lower() == 'sim':
+    print(f'{nome} Passou!')
+elif aluno_passou.lower() == 'nao':
+    print(f'{nome} infelizmente não passou!')
+else:
+    print('Resposta inválida, digite "sim" ou "nao"')
